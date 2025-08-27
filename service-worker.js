034d270c21720e27f00d7b0794b15916b36bb928
JONATHAN LUCATTO <j91636247@gmail.com>
2025-08-27 07:51:23 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,35 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
+const CACHE_NAME = 'robojl-cache-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Instalação do SW
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
 });
-self.addEventListener('activate', e => {
-  clients.claim();
+
+// Ativação do SW
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    ).then(() => self.clients.claim())
+  );
 });
-self.addEventListener('fetch', e => {
-  // Aqui você pode adicionar cache se quiser
+
+// Intercepta fetch
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
