27e1ea70c8f7d9a0672e136de447f193a693048e
JONATHAN LUCATTO <j91636247@gmail.com>
2025-09-03 21:49:42 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,24 @@
-self.addEventListener('install', e => {
+const CACHE_NAME = "robojl-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open('robojl-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(resp => {
-      return resp || fetch(e.request);
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
     })
   );
 });
