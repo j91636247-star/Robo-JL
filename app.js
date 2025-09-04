9caf9e90a0594e7fbb35a72af6b822df7fa790c0
JONATHAN LUCATTO <j91636247@gmail.com>
2025-09-03 21:29:58 -0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-// Registrar o service worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("service-worker.js")
-      .then(() => console.log("Service Worker registrado com sucesso"))
-      .catch(err => console.log("Erro ao registrar Service Worker", err));
-  });
-}
